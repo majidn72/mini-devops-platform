3c09d1668f33a66676904e390434af1786ca97b2
Majid <majid.naeeniyan@gmail.com>
2026-06-12 15:20:25 +0330

    add backend healthcheck and restart policy

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,9 @@ def home():
 
     except Exception as e:
         return f"Database Error: {e}"
+@app.route("/health")
+def health():
+    return "OK" , 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
